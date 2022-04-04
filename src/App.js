57ed96898e6475c8c196fe85655b77c917c6a976
changsusan <90660832+changsusan@users.noreply.github.com>
2022-04-04 19:47:59 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        h1 > Hello < /h1> <Weather / >
+        <
+        /header>{" "} <
+        /div>
+    );
+}
+
+export default App;
