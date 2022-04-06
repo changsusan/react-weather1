f937e71f28d40f08f58eed9fa2df8d0182b8c4f9
changsusan <90660832+changsusan@users.noreply.github.com>
2022-04-06 16:09:48 -0400

    json

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,11 @@
 import React from "react";
+import axios from "axios";
 export default function Weather() {
+    function handleResponse(response) {
+        alert(`The weather in New York is ${response.data.main.temp}°C`);
+    }
+    let apiKey = "8bfbf28022488dee7340fe98270ce789";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
     return <h2 > Hello from Weather < /h2>;
 }
